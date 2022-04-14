70bdf8bc20a350f097f88f979a61e23c4cb72a20
Aureo Dantax <dantasaureo19@gmail.com>
2022-04-13 22:23:29 -0300

    Trataments of Exceptions in UserResponse, controllers and services

--- uniproof-user-client/src/main/java/br/com/uniproof/integracao/userClient/client/UserResponse.java
+++ uniproof-user-client/src/main/java/br/com/uniproof/integracao/userClient/client/UserResponse.java
@@ -1,12 +1,17 @@
 package br.com.uniproof.integracao.userClient.client;
 
+import com.fasterxml.jackson.annotation.JsonInclude.*;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
 import lombok.Data;
 
+@Builder
 @Data
 public class UserResponse {
-    private Integer statuscode;
+    private Integer statusCode;
     private String message;
     private String token;
     private String companyToken;
     private Integer termsOfUseVersion;
+
 }
