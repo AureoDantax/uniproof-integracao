66ed77f57ce825ac5ed193943bca5feed1434708
Aureo Dantax <dantasaureo19@gmail.com>
2022-04-13 22:31:16 -0300

    Revert "Trataments of Exceptions in UserResponse, controllers and services"
    This reverts commit 70bdf8bc20a350f097f88f979a61e23c4cb72a20.

--- uniproof-user-client/src/main/java/br/com/uniproof/integracao/userClient/controllers/UserController.java
+++ uniproof-user-client/src/main/java/br/com/uniproof/integracao/userClient/controllers/UserController.java
@@ -4,26 +4,22 @@ import br.com.uniproof.integracao.userClient.client.User;
 import br.com.uniproof.integracao.userClient.client.UserResponse;
 import br.com.uniproof.integracao.userClient.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping ("user")
 public class UserController {
     @Autowired
     private UserService userservice;
 
-    @PostMapping("/login")
-    public ResponseEntity<UserResponse> getLoginToken(@RequestBody User user)  {
+    @PostMapping
+    public UserResponse getUser(){
+        return userservice.getUser();
 
-        UserResponse userResponse = userservice.getLoginToken(user.getEmail(), user.getPassword());
-        if (userResponse.getStatusCode() == null) {
-            return ResponseEntity.ok(userResponse);
-        } else {
-            return ResponseEntity.status(userResponse.getStatusCode()).body(userResponse);
-        }
     }
+
+
+
 }
